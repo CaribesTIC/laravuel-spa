014c6cea34aceaae33a00d670df4e7f875709505
ejimenez123 <caribestic123@gmail.com>
2022-06-11 17:17:10 -0400

    test: HttpUtil and UserService mock

--- tests/async/example.spec.ts
+++ tests/async/example.spec.ts
@@ -11,8 +11,9 @@ test('should fetch login-auth-user via http-auth-service', async () => {
     { name: 'Pineapple Gum', image: 'pineapple-gum.jpg', price: 3 },
     { name: 'Pineapple T-Shirt', image: 'pineapple-tshirt.jpg', price: 12 }
   ])
+  
+  const updateUser = await AuthService.updateUser()
+  
+  expect(updateUser).toEqual("User updated.");
     
 });
-
-
-
