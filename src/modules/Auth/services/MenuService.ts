f6e9d38619d56e95da6c2f146429cc6932ead458
ejimenez123 <caribestic123@gmail.com>
2022-06-17 15:20:24 -0400

    chore: Studying how to paginate the menu list

--- src/modules/Auth/services/MenuService.ts
+++ src/modules/Auth/services/MenuService.ts
@@ -6,6 +6,10 @@ export const getMenus = () => {
   return Http.get(path);
 }
 
+/*export const getMenus = () => {  
+  return Http.get(`${path}?page=1`);
+}*/
+
 export const getMenusChildren = (menuId) => {
   return Http.get(`${path}/children/${menuId}`);
 }
