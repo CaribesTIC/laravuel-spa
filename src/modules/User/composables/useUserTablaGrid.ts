82c329558bbb94947ce465abd609d0717e5f3a7e
ejimenez123 <caribestic123@gmail.com>
2022-07-20 19:57:06 -0400

    refactor: User TablaGrid part two

--- src/modules/User/composables/useUserTablaGrid.ts
+++ src/modules/User/composables/useUserTablaGrid.ts
@@ -1,37 +1,98 @@
-import { onMounted } from "vue";
-import { onBeforeRouteUpdate } from 'vue-router'
+import { reactive, onMounted } from "vue";
+import { useSearch } from "@/composables/useTableGrid";
+import { onBeforeRouteUpdate, useRouter, useRoute } from 'vue-router'
+import { useHttp } from "@/composables/useHttp";
 import UserService from "../services";
+import type User from "../types/User"
+
 export function useUserTablaGrid() {
 
-const getUsers = (routeQuery: string) => {  
-  return UserService.getUsers(routeQuery)
-    .then((response) => {
-      errors.value = null
-      data.rows = response.data.rows.data
-      data.links = response.data.rows.links
-      data.search = response.data.search
-      data.sort = response.data.sort
-      data.direction = response.data.direction      
+  const data = reactive({
+    rows: [] as User[],
+    links: [],
+    search: "",
+    sort: "",
+    direction: ""
+  })
+
+  const {  
+    errors,
+    getError     
+  } = useHttp()
+
+  const router = useRouter();
+  const route = useRoute();
+
+  const load = (newParams: object) => {
+    const params = {
+      search: data.search || "",
+      sort: data.sort || "",
+      direction: data.direction || "",
+      ...newParams,
+    }
+
+    router.push({
+      path:'/users',
+      query: {
+        ...route.query,
+        ...params
+      }
     })
-    .catch((error) => {
-      console.log(error)
-    })    
-}
-
-onBeforeRouteUpdate(async (to, from) => {      
-  if (to.query !== from.query) {        
-    await getUsers(new URLSearchParams(to.query).toString());        
   }
-})
 
-onMounted(() => {
-  getUsers(new URLSearchParams(route.query).toString());    
-})
+  const { 
+    setSearch,
+    setSort, 
+  } = useSearch(data, load)
 
+  const getUsers = (routeQuery: string) => {  
+    return UserService.getUsers(routeQuery)
+      .then((response) => {
+        errors.value = null
+        data.rows = response.data.rows.data
+        data.links = response.data.rows.links
+        data.search = response.data.search
+        data.sort = response.data.sort
+        data.direction = response.data.direction      
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  const deleteRow = (rowId?: string) => {
+    if (rowId === undefined)
+      return
+    else if (confirm(`¿Estás seguro que desea eliminar el registro ${rowId}?`)) {    
+      return UserService.deleteUser(rowId)
+        .then((response) => {
+          errors.value = null
+          router.push( { path: '/users' } )        
+        })
+        .catch((err) => {                
+          console.log( err.response.data )
+          errors.value = getError(err)
+        })
+    }
+  }
+
+  onBeforeRouteUpdate(async (to, from) => {      
+    if (to.query !== from.query) {        
+      await getUsers(new URLSearchParams(to.query).toString())
+    }
+  })
+
+  onMounted(() => {
+    getUsers(new URLSearchParams(route.query).toString())
+  })
 
   return {
-
-  };
-
+    errors,
+    data,
+    deleteRow,
+    router,  
+    setSearch,
+    setSort
+  }
 }
 
--- src/modules/User/views/Index.vue
+++ src/modules/User/views/Index.vue
@@ -1,92 +1,17 @@
 <script setup lang="ts">
-import { onMounted, reactive } from "vue";
-import { onBeforeRouteUpdate, useRouter, useRoute } from 'vue-router' 
-import { useSearch } from "@/composables/useTableGrid";
-import { useHttp } from "@/composables/useHttp";
+import { useUserTablaGrid } from "../composables/useUserTablaGrid";
 import AppPaginationB from "@/components/AppPaginationB.vue";
 import AppPageHeader from "@/components/AppPageHeader.vue"
 import AppBtn from "@/components/AppBtn.vue"
-import UserService from "../services";
-import type User from "../types/User"
 
-const router = useRouter();
-const route = useRoute();
-
-const {  
-  errors,  
-  getError     
-} = useHttp()
-
-const data = reactive({
-  rows: [] as User[],
-  links: [],
-  search: "",
-  sort: "",
-  direction: ""
-})
-
-const load = (newParams: object) => {
-  const params = {
-    search: data.search || "",
-    sort: data.sort || "",
-    direction: data.direction || "",
-    ...newParams,
-  }
-
-  router.push({
-    path:'/users',
-    query: {
-      ...route.query,
-      ...params
-    }
-  })
-}
-
-const { 
+const {
+  errors,
+  data,
+  deleteRow,
+  router,  
   setSearch,
-  setSort, //setFilter
-} = useSearch(data, load)
-
-const getUsers = (routeQuery: string) => {  
-  return UserService.getUsers(routeQuery)
-    .then((response) => {
-      errors.value = null
-      data.rows = response.data.rows.data
-      data.links = response.data.rows.links
-      data.search = response.data.search
-      data.sort = response.data.sort
-      data.direction = response.data.direction      
-    })
-    .catch((error) => {
-      console.log(error)
-    })    
-}
-
-const deleteRow = (rowId?: string) => {
-  if (rowId === undefined)
-    return
-  else if (confirm(`¿Estás seguro que desea eliminar el registro ${rowId}?`)) {    
-    return UserService.deleteUser(rowId)
-      .then((response) => {
-        errors.value = null
-        router.push( { path: '/users' } )        
-      })
-      .catch((err) => {                
-        console.log( err.response.data )
-        errors.value = getError(err)
-      })
-  }
-}
-
-onBeforeRouteUpdate(async (to, from) => {      
-  if (to.query !== from.query) {        
-    await getUsers(new URLSearchParams(to.query).toString());        
-  }
-})
-
-onMounted(() => {
-  getUsers(new URLSearchParams(route.query).toString());    
-})
+  setSort  
+} = useUserTablaGrid()
 </script>
 
 <template>
