af517a7a901609c2ec20869d5e2615a11035d6d4
Ernesto Canquiz <fdsoil123@gmail.com>
2023-01-19 19:24:25 -0500

    style: Improving home, login, and register

--- src/modules/Auth/views/Home.vue
+++ src/modules/Auth/views/Home.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<!--script setup lang="ts">
   import IconLogo from "@/icons/IconLogo.vue"
 </script>
 
@@ -22,4 +22,47 @@
       </AppLink>      
     </div>
   </div>  
+</template-->
+
+
+<script setup lang="ts">
+import IconLogo from "@/icons/IconLogo.vue"
+</script>
+
+<template>
+  <div class="m-10 sm:my-20 bg-base-100">
+    <div class="sm:grid grid-cols-2 gap-4">
+      <div class="my-auto"><IconLogo class= "w-full h-full"/></div>
+      <div class="my-auto">
+        <div class="">
+          <div class="text-[#6C63FF] wx-full mx-auto grid justify-items-stretch my-3">
+            <p class="text-xl justify-self-center">Backend / Frontend</p>
+          </div>
+          <div class="text-current wx-full mx-auto grid justify-items-stretch my-3">
+            <p class="text-3xl justify-self-center">LaraVuel [api*spa]</p>
+          </div>
+          <div class="w-full wx-full wx-full mx-auto grid justify-items-stretch my-3">
+            <p class="text-lg dark:text-gray-300 justify-self-center">Laravel & Vue (Scaffolding)</p>
+          </div>
+          <div class="flex justify-center text-lg my-10 space-x-5">
+            <AppLink
+              to="/login"
+              class="btn btn-primary"
+              data-testid="login-link"
+            >
+              Iniciar Sesión
+            </AppLink>
+            <AppLink
+              to="/register"
+              class="btn btn-default"
+              data-testid="register-link"
+            >
+              Registrarse
+            </AppLink>
+          </div>
+        </div>
+      </div>      
+    </div>
+  </div>
 </template>
+
--- src/modules/Auth/views/Login.vue
+++ src/modules/Auth/views/Login.vue
@@ -8,10 +8,10 @@
 
 <template>
   <div class="p-5 m-auto w-full sm:w-4/12">
-    <IconLogo class= "flex justify-center pt-4 sm:justify-start sm:pt-0 w-1/3 y-1/3 h-auto m-auto bg-transparent"/>
-    <h2 class="my-4 text-xl text-center">Inicio de Sesión</h2>
+    <IconLogo class= "flex justify-center pt-4 sm:justify-start sm:pt-0 w-1/4 y-1/4 h-auto m-auto bg-transparent"/>
+    <h2 class="my-4 text-2xl text-center">Inicio de Sesión</h2>
     <FormLogin
-      class="p-5 bg-white border rounded shadow"
+      class="p-5 bg-base-200 border rounded shadow"
       @submit='login($event)'      
       :sending='sending'
       :error='error'
